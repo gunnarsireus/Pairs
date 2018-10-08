b74b12a3c1418fa4fe1884b8aa6580fc2806d6de
gunnarsireus <gunnar.sireus@gmail.com>
2018-10-08 12:38:30 +0200

    Updated according to specification

--- Pairs/Program.cs
+++ Pairs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Pairs
@@ -7,12 +8,45 @@ namespace Pairs
     {
         static void Main(string[] args)
         {
-            CountNumberOfPairsWithDiff(5, 2, new[] { 1, 5, 3, 4, 2 });
+            string input;
+            Console.Write("Input n and k: ");
+            input =  Console.ReadLine();
+            var inputArr = input.Split(" ");
+            int n = int.Parse(inputArr[0]);
+            int k = int.Parse(inputArr[1]);
+            Console.Write("Input array: ");
+            input = Console.ReadLine();
+            var arr = input.Split(" ");
+            int[] intArr= new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                intArr[i] = int.Parse(arr[i]);
+            }
+
+            Console.WriteLine("Number of Pairs: " + CountNumberOfPairsWithDiff(n, k, intArr));
             Console.ReadKey();
         }
 
         public static int CountNumberOfPairsWithDiff(int n, int k, int[] arr)
         {
+            if (n != arr.Length)
+            {
+                Console.WriteLine("Array size error");
+            }
+
+            if (n < 2)
+            {
+                Console.WriteLine("Array too small, must be of size 2 or bigger!");
+            }
+            if (n > 100000)
+            {
+                Console.WriteLine("Array too big, must not be bigger than 100000!");
+            }
+            var diff = arr.Max() - arr.Min();
+            if (arr.Max() - arr.Min() < k)
+            {
+                Console.WriteLine("No pair found! ");
+            }
             var numbers = arr.OrderBy(o => o).Select((value) => new { value });
 
             var pairs = from num1 in numbers
